8854ccbd6be8f98e9b461a88df2473d1003f6c4f
Miguel Jimenez <denkatalabeno@gmail.com>
2021-10-16 22:41:25 +0800

    created very basic intro scene

---
+++ src/assets/treasure-hunters/Terrain (32x32).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Terrain (32x32)" tilewidth="32" tileheight="32" tilecount="85" columns="17">
+ <image source="palm-tree-island/Sprites/Terrain/Terrain (32x32).png" width="544" height="160"/>
+</tileset>
